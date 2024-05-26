d647f746a1166ba03e8c1ca1712a0c0e5155bafd
drybonez01 <83592470+drybonez01@users.noreply.github.com>
2024-05-26 18:41:18 +0200

    refactored majority_influence_diffusion.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from methods.weightDef import weightDef
 from methods.seedset_alg1 import seedset_alg1
 from methods.seedset_alg2 import seedset_alg2
 from methods.seedset_alg3 import seedset_alg3
+from methods.majority_influence_diffusion import majority_influence_diffusion
 
 if len(sys.argv) > 1:
     if sys.argv[1] == "-w" or sys.argv[1] == "--weightDef":
@@ -27,24 +28,35 @@ if len(sys.argv) > 1:
             seedset_alg3(sys.argv[2], sys.argv[3])
         except:
             print("ERRORE. Almeno uno dei percorsi inseriti è errato o inesistente.")
+    elif sys.argv[1] == "-m" or sys.argv[1] == "--majority_influence_diffusion":
+        try:
+            majority_influence_diffusion(sys.argv[2], sys.argv[3])
+        except:
+            print("ERRORE. Almeno uno dei percorsi inseriti è errato o inesistente.")
+    else:
+        print("ERRORE. Parametro non valido. Per maggiori informazioni, utilizzare il seguente comando:\n\tpython "
+              "main.py -h")
+
 else:
-    graph_path = input("\nInserisci il percorso del file .gml: ")
-    while not os.path.exists(graph_path):
+    graph_file_path = input("\nInserisci il percorso del file .gml: ")
+    while not os.path.exists(graph_file_path):
         print("ERRORE. Percorso errato o inesistente. Riprovare")
-        graph_path = input("Inserisci il percorso del file .gml: ")
-    weights_file_path = weightDef(graph_path)
+        graph_file_path = input("Inserisci il percorso del file .gml: ")
+    weights_file_path = weightDef(graph_file_path)
 
     seedset_file_path = None
     while True:
         alg = int(input("\nInserire l'algoritmo da utilizzare per la selezione del seed set (1,2,3): "))
         if alg == 1:
-            seedset_file_path = seedset_alg1(graph_path, weights_file_path)
+            seedset_file_path = seedset_alg1(graph_file_path, weights_file_path)
             break
         elif alg == 2:
-            seedset_file_path = seedset_alg2(graph_path, weights_file_path)
+            seedset_file_path = seedset_alg2(graph_file_path, weights_file_path)
             break
         elif alg == 3:
-            seedset_file_path = seedset_alg3(graph_path, weights_file_path)
+            seedset_file_path = seedset_alg3(graph_file_path, weights_file_path)
             break
         else:
             print("Opzione non valida. Riprovare.")
+
+    majority_influence_diffusion(graph_file_path, seedset_file_path)
