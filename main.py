0b612b681425e4e1cec41ea2dd5eb10180b3cded
drybonez01 <83592470+drybonez01@users.noreply.github.com>
2024-05-26 17:07:30 +0200

    refactored weightDef.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import os
+import sys
+
 from weightDef import weightDef
 
-while True:
+'''while True:
     print("")
     print("1 - Definisci i pesi")
     print("0 - Termina")
@@ -12,4 +15,17 @@ while True:
         print("Arrivederci.")
         break
     else:
-        print("Operazione non valida. Riprovare.")
+        print("Operazione non valida. Riprovare.")'''
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == "-w" or sys.argv[1] == "--weightDef":
+        try:
+            weightDef(sys.argv[2])
+        except:
+            print("ERRORE. Percorso errato o inesistente.")
+else:
+    graph_path = input("Inserisci il percorso del file .gml: ")
+    while not os.path.exists(graph_path):
+        print("ERRORE. Percorso errato o inesistente. Riprovare")
+        graph_path = input("Inserisci il percorso del file .gml: ")
+    weights_file_path = weightDef(graph_path)
--- weightDef.py
+++ weightDef.py
@@ -4,7 +4,7 @@ import os
 import math
 
 
-def weightDef():
+def weightDef(graph_path):
     def load_graph(filename):
         graph = nx.read_gml(filename, label='id')
         return graph
@@ -32,45 +32,49 @@ def weightDef():
             for node_id, weight in weights.items():
                 f.write(f"{node_id} {weight}\n")
 
-    graph_path = input("Inserisci il percorso del file .gml: ")
+    # graph_path = input("Inserisci il percorso del file .gml: ")
 
     g = load_graph(graph_path)
 
-    print("Numero di nodi:", g.number_of_nodes())
+    print("\nNumero di nodi:", g.number_of_nodes())
     print("Numero di archi:", g.number_of_edges())
 
     '''for n in g.nodes():
         print(f"Nodo {n} ha {g.degree(n)} archi")'''
 
-    method = input("Scegli il metodo per assegnare i pesi ai nodi:\n1. Pesi randomici\n2. Pesi basati sul grado\n3. "
-                   "Pesi basati sulla centralità di betweenness\nInserisci 1, 2 o 3: ")
+    node_weights = None
+    file_suffix = None
+    while True:
+        method = input(
+            "\nScegli il metodo per assegnare i pesi ai nodi:\n1. Pesi randomici\n2. Pesi basati sul grado\n3. "
+            "Pesi basati sulla centralità di betweenness\nInserisci 1, 2 o 3: ")
 
-    if method == "1":
-        weight_min = int(input("Inserisci il valore minimo del range dei pesi: "))
-        weight_max = int(input("Inserisci il valore massimo del range dei pesi: "))
-        node_weights = assign_random_weights_to_networkx(g, (weight_min, weight_max))
-        file_suffix = "random"
-    elif method == "2":
-        node_weights = assign_degree_based_weights(g)
-        file_suffix = "degree_based"
-    elif method == "3":
-        node_weights = assign_betweenness_based_weights(g)
-        file_suffix = "betweenness_based"
-    else:
-        print("Metodo non valido. Uscita.")
-        return
+        if method == "1":
+            weight_min = int(input("\nInserisci il valore minimo del range dei pesi: "))
+            weight_max = int(input("Inserisci il valore massimo del range dei pesi: "))
+            node_weights = assign_random_weights_to_networkx(g, (weight_min, weight_max))
+            file_suffix = "random"
+            break
+        elif method == "2":
+            node_weights = assign_degree_based_weights(g)
+            file_suffix = "degree_based"
+            break
+        elif method == "3":
+            node_weights = assign_betweenness_based_weights(g)
+            file_suffix = "betweenness_based"
+            break
+        else:
+            print("Metodo non valido. Riprovare.")
 
-    print("\nPesi assegnati ai nodi:")
-    '''for n_id, w in node_weights.items():
+    '''print("\nPesi assegnati ai nodi:")
+    for n_id, w in node_weights.items():
         print(f"Nodo {n_id}: Peso {w}")'''
 
     graph_name = os.path.splitext(os.path.basename(graph_path))[0]
     weights_file_name = f"{graph_name}_node_weights_{file_suffix}.txt"
-
     weights_file_path = os.path.join('risorse', 'weights', weights_file_name)
-
     os.makedirs(os.path.dirname(weights_file_path), exist_ok=True)
-
     save_weights_to_file(node_weights, weights_file_path)
 
-    print(f"Pesi salvati in: {weights_file_path}")
+    print(f"\nPesi salvati in: {weights_file_path}")
+    return weights_file_path
